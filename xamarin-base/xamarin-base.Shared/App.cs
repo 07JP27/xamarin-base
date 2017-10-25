4c995ee9c0c3bdb6d8e4bba267582bced1f15f7a
07JP27 <jynpei.t@gmail.com>
2017-10-25 16:44:52 +0900

    initial commit

---
+++ xamarin-base/xamarin-base.Shared/App.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+using xamarinbase.Views;
+
+namespace xamarinbase
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            var content = new MyPage();
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
